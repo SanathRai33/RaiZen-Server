97051314895e2a0d9eedc02934b073fbf8746ad3
SanathRai <sanathrai03@gmail.com>
2025-05-28 23:48:46 +0530

    middleware implemented for user auth

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const protect = async (req, res, next) => {
+  let token = req.headers.authorization;
+
+  if (token && token.startsWith('Bearer')) {
+    try {
+      token = token.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET || 'your_jwt_secret');
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      res.status(401).json({ message: 'Unauthorized, invalid token' });
+    }
+  } else {
+    res.status(401).json({ message: 'No token provided' });
+  }
+};
+
+module.exports = protect;
